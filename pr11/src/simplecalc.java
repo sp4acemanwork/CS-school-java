f957ecd9fca942e28363cf78bc149143749ee5ea
sp4aceman-school <js079077@sdale.org>
2022-02-06 02:21:45 -0600

    new

---
+++ pr11/src/simplecalc.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+
+public class simplecalc {
+    JFrame f;
+    JTextField t1;
+    JTextField t2;
+    JTextField t3;
+    JLabel l1;
+    JLable l2;
+    JLable l3;
+    JButton b1;
+    JButton b2;
+    JButton b3;
+    JButton b4;
+    JButton b5;    
+
+    public simplecalc(){
+        f= new JFrame("simple calc");
+        f.setLayout(600,600);
+        f.setLayout(null);
+        f.setVisible(true);
+        int num1 = null;
+        int num2 = null;
+        int result = null;
+
+        //f.getContentPane().setBackground();
+        
+
+
+
+
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
