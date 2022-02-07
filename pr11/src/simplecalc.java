dbc5c4af1a6acc42bf05b9beae1d6c8bd2b14944
sp4aceman-school <js079077@sdale.org>
2022-02-06 22:51:24 -0600

    for the teacher

--- pr11/src/simplecalc.java
+++ pr11/src/simplecalc.java
@@ -37,7 +37,7 @@ l1.setBounds(15,h-h,100,25);
 t1= new JTextField("");
 t1.setBounds(15,h-h+25,100,25);
 
-tiads = new JLabel("?");
+tiads = new JLabel("?"); // litle touch to add some polish 
 tiads.setBounds(120,h-h+25,10,25);
 //
 l2 = new JLabel("num 2");
