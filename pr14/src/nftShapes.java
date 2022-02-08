e0a2253f316bed4ef9737c3fd9be93ad3a96aa62
sp4aceman-school <js079077@sdale.org>
2022-02-08 15:00:54 -0600

    sudo code for creating graphics
    sudo code for creating basic graphics
    will be used for all 4 java apps

--- pr14/src/nftShapes.java
+++ pr14/src/nftShapes.java
@@ -1,8 +1,41 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.lang.reflect.Array;
+import java.util.function.Function;
 
 public class nftShapes {
+
+public nftShapes(){
+
+
+// SUDO CODE 
+/* this programm functions by useing classes to create graphics objects 
+then randomizes the paramiters useing the Math.random funtion 
+seting possition is tbh wether or not that will randomized as well */
+
+
+
+}
+
+
+
+// class for createing a box
+private void createBox(int w,int h,boolean f,int[] c ) {
+        
+}
+// class for createing a circle
+private void createCircle(int w,int h,boolean f,int[] c ) {
+        
+}
+// class for createing a triangle
+private void createtriangle(int w,int h,boolean f,int[] c ) {
+        
+}
+
+
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
