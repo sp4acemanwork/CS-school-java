c2ba2122a01613b55be6c16900e4aa4a99d0c38d
sp4aceman-school <js079077@sdale.org>
2022-02-08 01:31:01 -0600

    added full functionality and some polish prob need some more

--- pr13/src/guessingGame.java
+++ pr13/src/guessingGame.java
@@ -3,7 +3,11 @@ import java.awt.event.*;
 import java.awt.*;
 
 
+
+
 public class guessingGame {
+    private double  rand;
+    private int trys, num1,num2;
     JFrame f;
     JTextField t1;
     JTextField t2;
@@ -38,10 +42,9 @@ t1.setBounds(130,h-h+25,100,25);
 
 //
 l2 = new JLabel("Enter a number");
-l2.setBounds(240,h-h+25,100,25);
+l2.setBounds(240,h-h+25,200,25);
 //
-t2= new JTextField("");
-t2.setBounds(350,h-h+25,100,25);
+
 //
 
 
@@ -49,50 +52,55 @@ f.add(l1);
 f.add(l2);
 
 f.add(t1);
-f.add(t2);
+
 
 
 
 
 
 //---------------------buttons------------------------------
-        b1 = new JButton("guess");
+        b1 = new JButton("Guess");
         b1.setBounds(w/8,h-h+120,100,25);
         f.add(b1);
-
+        
+        
+        b2 = new JButton("New game");
+        b2.setBounds(w/8,h-h+150,100,25);
+        f.add(b2);
 
 //----------------------------------------------------
-       
+
         // guess event 
         b1.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                double num1 = Double.parseDouble(t1.getText());
-                
-               if(){
+            num1 = Integer.parseInt(t1.getText());
+            
+            
 
-               }else if (condition) {
-                   
-               } else if(condition) {
-                   
+               if(num1>num2){
+                    l2.setText("guess lower");
+                    trys +=1;
+               }else if (num1<num2) {
+                   l2.setText("guess higher");
+                   trys +=1;
+               } else if(num1==num2) {
+                    trys +=1;
+                   l2.setText("You Win!!! it took you " + trys + " trys");
                } else{
                    l2.setText("ERROR");
                }
-                
-                
-                
-                
-                
-                l2.setText("Ferignhight");
-               
-                
-             
-                
-
-
+            }
+            
+        });
+        // new game event
+        b2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                t1.setText("");
+                l2.setText("make a guess");
+                rand = Math.random()*100;
+                num2 = (int) rand;  
             }
         });
-        // cel event 
- 
 
 
 
